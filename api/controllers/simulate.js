5cf349dc3a0da924bf8da62cf5d8ba158697de6a
orange-na <d-0133@tsuyama.kosen-ac.jp>
2023-08-19 12:47:29 -0700

    ✨ data + sum + values

--- api/controllers/simulate.js
+++ api/controllers/simulate.js
@@ -4,27 +4,30 @@ const jwt = require("jsonwebtoken");
 
 const getOrder = (req, res) => {
   // ランダムに3つの値を選び、その合計が1000以下になるデータを取得するクエリ
-  const query = `
-      SELECT cost
+  const q = `
+      SELECT *
       FROM posts  
-      WHERE cost <= 1000
+      WHERE cost <= $1
       ORDER BY random()
-      LIMIT 3;
+      LIMIT $2;
     `;
 
+  const values = [req.body.limit, req.body.term];
+
   // クエリを実行して結果を取得
-  pool.query(query, (err, result) => {
+  pool.query(q, [1000, 3], (err, result) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: "An error occurred" });
     } else {
       // クエリ結果から値を取得し、合計を計算
+      const data = result.rows;
       const selectedValues = result.rows.map((row) => row.cost);
       const total = selectedValues.reduce((sum, cost) => sum + cost, 0);
 
       // 合計が1000以下の場合、値と合計を返す
       if (total <= 1000) {
-        res.json({ selectedValues, total });
+        res.json({ selectedValues, total, data });
       } else {
         // 合計が1000を超える場合は再度クエリを実行
         res.redirect("http://localhost:8800/api/simulate/getorder");
